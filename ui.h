07a5d633c4f0ed7b5bb525cf8bb93613ff02a669
wallyS <rwspicer@alaska.edu>
2013-04-02 18:08:37 -0800

    initial version of ui.h

---
+++ ui.h
@@ -0,0 +1,22 @@
+//UI.h
+// ross spicer
+//
+// ver 1: contains the declerations of the 
+// functions that will likely be used in the UI Class 
+#ifndef UI_HEADER_FILE
+#define UI_HEADER_FILE
+
+class UI
+{
+public:
+	void draw();
+	void update();
+	void mouseDown( MouseEvent event );
+	void keyDown( KeyEvent event );
+}
+
+
+
+
+
+#endif
