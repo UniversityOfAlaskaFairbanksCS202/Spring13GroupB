22e03baac5e87c3feefb08325dc96a4cc254ac81
wallyS <rwspicer@alaska.edu>
2013-04-29 16:38:20 -0800

    ui fixed for death

--- ui.cpp
+++ ui.cpp
@@ -8,87 +8,90 @@ void  UI::keyDown( char key, Dungeon & myDungeon , Player & myPlayer, ImportantM
 {
 
 	const int EXIT_TIME = 3;
-	
 	if (key == '`' )
 		{
 			exit(0);
 		}
-	// ui for movement
-	if (!myDungeon.occupied())
+
+	if (myPlayer.getHitPoints() > 0)
 	{
-		if ((key == 'w')||(key == 'a')||(key == 'd'))
+		// ui for movement
+		if (!myDungeon.occupied())
 		{
-			bool moved;
-			moved = myDungeon.ChangeRoom(key, myPlayer.getLevel());
-			if (moved)
+			if ((key == 'w')||(key == 'a')||(key == 'd'))
+			{
+				bool moved;
+				moved = myDungeon.ChangeRoom(key, myPlayer.getLevel());
+				if (moved)
+					{
+					myPlayer.movementDescription();
+					}
+		
+			}
+			if (key == 'h')
+			{
+				myPlayer.drinkHPPotion();
+			}
+			if (key == 'g')
+			{
+				myPlayer.drinkManaPotion();
+			}
+
+		}
+		else
+		{
+			Player::fightVictor victor;
+			if (key == ' ')
+			{
+			
+				victor = myPlayer.turn(myDungeon.getMonster(),Player::ATTACK);
+					if (victor == Player::PLAYER)
+						{
+						myPlayer.enemyDrop(myDungeon.getMonster());
+						myDungeon.killMonster();
+						}
+					if (victor == Player::MONSTER)
+						{
+							splash.splashMessage("You've Just Died,\nthe Game will now exit", EXIT_TIME, true);
+						}
+			}
+			if (key == 'm')
+			{
+				victor = myPlayer.turn(myDungeon.getMonster(),Player::MAGICATTACK);
+					if (victor == Player::PLAYER)
+						{
+						myPlayer.enemyDrop(myDungeon.getMonster());
+						myDungeon.killMonster();
+						}
+					if (victor == Player::MONSTER)
+						{
+							splash.splashMessage("You've Just Died,\r\nthe Game will now exit", EXIT_TIME, true);
+						}
+			}
+			if (key == 'h')	
+			{
+				victor = myPlayer.turn(myDungeon.getMonster(),Player::DRINKHP);
+					if (victor == Player::MONSTER)
+						{
+						splash.splashMessage("You've Just Died,\nthe Game will now exit", EXIT_TIME, true);
+						}
+			}
+			if (key == 'g')
+			{
+				victor = myPlayer.turn(myDungeon.getMonster(),Player::DRINKMANA);
+					if (victor == Player::MONSTER)
+						{
+							splash.splashMessage("You've Just Died,\nthe Game will now exit", EXIT_TIME, true);
+						}
+
+			}
+			if (key == '0')
 				{
-				myPlayer.movementDescription();
+					myPlayer.enemyDrop(myDungeon.getMonster());
+					myDungeon.killMonster();
 				}
 		
 		}
-		if (key == 'h')
-		{
-			myPlayer.drinkHPPotion();
 		}
-		if (key == 'g')
-		{
-			myPlayer.drinkManaPotion();
-		}
-
-	}
-	else
-	{
-		Player::fightVictor victor;
-		if (key == ' ')
-		{
-			
-			victor = myPlayer.turn(myDungeon.getMonster(),Player::ATTACK);
-				if (victor == Player::PLAYER)
-					{
-					myPlayer.enemyDrop(myDungeon.getMonster());
-					myDungeon.killMonster();
-					}
-				if (victor == Player::MONSTER)
-					{
-						splash.splashMessage("You've Just Died,\nthe Game will now exit", EXIT_TIME, true);
-					}
-		}
-		if (key == 'm')
-		{
-			victor = myPlayer.turn(myDungeon.getMonster(),Player::MAGICATTACK);
-				if (victor == Player::PLAYER)
-					{
-					myPlayer.enemyDrop(myDungeon.getMonster());
-					myDungeon.killMonster();
-					}
-				if (victor == Player::MONSTER)
-					{
-						splash.splashMessage("You've Just Died,\r\nthe Game will now exit", EXIT_TIME, true);
-					}
-		}
-		if (key == 'h')
-		{
-			victor = myPlayer.turn(myDungeon.getMonster(),Player::DRINKHP);
-				if (victor == Player::MONSTER)
-					{
-						splash.splashMessage("You've Just Died,\nthe Game will now exit", EXIT_TIME, true);
-					}
-		}
-		if (key == 'g')
-		{
-			victor = myPlayer.turn(myDungeon.getMonster(),Player::DRINKMANA);
-				if (victor == Player::MONSTER)
-					{
-						splash.splashMessage("You've Just Died,\nthe Game will now exit", EXIT_TIME, true);
-					}
-
-		}
-		if (key == '0')
-			{
-				myPlayer.enemyDrop(myDungeon.getMonster());
-				myDungeon.killMonster();
-			}
-		
-	}
 	
 }
